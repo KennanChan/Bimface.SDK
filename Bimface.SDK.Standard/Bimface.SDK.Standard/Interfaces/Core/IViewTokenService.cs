782793a4cc10600e9636a9df1b6c5a95a295f0a5
KennanChan <chenkn@glodon.com>
2019-04-08 13:43:37 +0800

    add viewtokenservice summary

--- Bimface.SDK.Standard/Bimface.SDK.Standard/Interfaces/Core/IViewTokenService.cs
+++ Bimface.SDK.Standard/Bimface.SDK.Standard/Interfaces/Core/IViewTokenService.cs
@@ -11,10 +11,33 @@ namespace Bimface.SDK.Interfaces.Core
     {
         #region Others
 
-        Task<string> FetchCompareViewToken(FetchCompareViewTokenParameter             parameter);
-        Task<string> FetchFileViewToken(FetchFileViewTokenParameter                   parameter);
+        /// <summary>
+        ///     Fetch a view token for a compare model
+        /// </summary>
+        /// <param name="parameter">The parameter required for the request</param>
+        /// <returns>The view token</returns>
+        Task<string> FetchCompareViewToken(FetchCompareViewTokenParameter parameter);
+
+        /// <summary>
+        ///     Fetch a view token for a file model
+        /// </summary>
+        /// <param name="parameter">The parameter required for the request</param>
+        /// <returns>The view token</returns>
+        Task<string> FetchFileViewToken(FetchFileViewTokenParameter parameter);
+
+        /// <summary>
+        ///     Fetch a view token for a sub file of an integrate model
+        /// </summary>
+        /// <param name="parameter">The parameter required for the request</param>
+        /// <returns>The view token</returns>
         Task<string> FetchIntegrateFileViewToken(FetchIntegrateFileViewTokenParameter parameter);
-        Task<string> FetchIntegrateViewToken(FetchIntegrateViewTokenParameter         parameter);
+
+        /// <summary>
+        ///     Fetch a view token for an integrate model
+        /// </summary>
+        /// <param name="parameter">The parameter required for the request</param>
+        /// <returns>The view token</returns>
+        Task<string> FetchIntegrateViewToken(FetchIntegrateViewTokenParameter parameter);
 
         #endregion
     }
